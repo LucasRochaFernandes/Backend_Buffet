8d9d97298cd0d85fbc3afbc33b43d7e304d3a07e
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2022-03-12 23:20:22 -0300

    products_order

---
+++ src/modules/Orders/infra/entities/ProductsOrder.ts
@@ -0,0 +1,47 @@
+import { Inventory } from "@modules/Products/infra/entities/Inventory";
+import { v4 as uuidV4 } from "uuid";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+} from "typeorm";
+import { Order } from "./Order";
+
+@Entity("products_order")
+export class ProductsOrder {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  order_id: string;
+
+  @ManyToOne(() => Order)
+  @JoinColumn({ name: "order_id" })
+  order: Order;
+
+  @Column()
+  product_id: string;
+
+  @ManyToOne(() => Inventory)
+  @JoinColumn({ name: "product_id" })
+  product: Inventory;
+
+  @Column()
+  product_amount: number;
+
+  @Column()
+  total_product_price: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
